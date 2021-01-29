f0ec3ea86ebe015b045b1d891049dabb0ee6d477
M0chizuki <mochizuki@belzeen.net>
2021-01-29 09:37:46 -0300

    Just main things

--- Revisao 2 - Os Bravos e Destemidos/src/main.c
+++ Revisao 2 - Os Bravos e Destemidos/src/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
 #include "argparse.h"
 #include "path.h"
 #include "dataStore.h"
 #include "commandParse.h"
 
-int main() {
-
-    dataStoreT *dataStore = createDataStore(NULL, NULL, NULL, NULL);
+int main(int argc, char *argv[]) {
+    dataStoreT *dataStore = createDataStore("./", NULL, NULL, NULL);
+    FILE *f = fopen("inputFile", "r");
+    while (!feof(f)) {
+        commandParse(f, dataStore);
+    }
+    
     return 0;
 }
