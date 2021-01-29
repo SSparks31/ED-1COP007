1b6ff8c1894a8d4188124ee906f74700fb331643
M0chizuki <mochizuki@belzeen.net>
2021-01-29 09:38:01 -0300

    String manipulation module

---
+++ Revisao 2 - Os Bravos e Destemidos/src/stringManip.h
@@ -0,0 +1,9 @@
+#ifndef MODULO_STRINGMANIP_H
+#define MODULO_STRINGMANIP_H
+
+char* findCharacter(char *string, char query);
+char* rfindCharacter(char *string, char query);
+
+int validString(char* string);
+
+#endif
