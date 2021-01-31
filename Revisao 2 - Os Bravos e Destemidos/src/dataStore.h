e08d3bf098637aa03b637798f58fe8c03d087466
M0chizuki <mochizuki@belzeen.net>
2021-01-31 11:21:02 -0300

    Add comments

--- Revisao 2 - Os Bravos e Destemidos/src/dataStore.h
+++ Revisao 2 - Os Bravos e Destemidos/src/dataStore.h
@@ -5,9 +5,21 @@ typedef struct dataStore dataStoreT;
 typedef struct dataFile dataFileT;
 
 dataStoreT *createDataStore(char *defaultDir, int *parameters);
+/* Cria estrutura responsavel por armazenar variaveis e parametros do programa */
+/* Caso `defaultDir` seja um caminho valido, inicializa diretorio atual do programa com seu valor */
+/* Caso haja problema de alocacao de memoria, retorna NULL */
+
 void destroyDataStore(dataStoreT **dataStore);
+/* Destroi estrutura do programa */
+/* Substitui ponteiro original por NULL para fim de controle */
 
 dataFileT *createDataFile(int id, char *path, char *mode);
+/* Cria estrutura responsavel por armazenar stream de arquivo e modo */
+/* Caso haja problema de alocacao de memoria, retorna NULL */
+/* Caso nao seja possivel abrir o arquivo denominado por `path`, retorna NULL*/
+
 void destroyDataFile(dataFileT **dataFile);
+/* Destroi estrutura do arquivo */
+/* Substitui ponteiro original por NULL para fim de controle */
 
 #endif
