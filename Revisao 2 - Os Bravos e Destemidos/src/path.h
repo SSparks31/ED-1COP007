bdac883f2d788f5fba11544c1ff5d8ed13e35bbf
M0chizuki <mochizuki@belzeen.net>
2021-01-31 14:22:04 -0300

    Minor comment change

--- Revisao 2 - Os Bravos e Destemidos/src/path.h
+++ Revisao 2 - Os Bravos e Destemidos/src/path.h
@@ -8,14 +8,14 @@ char *getPath(char *res, char *fullPath);
 /* Dados uma string valida `res` e um nome de arquivo possivelmente completamente qualificado `fullPath`, armazena o seu caminho, sem '/' no final, em `res` */
 /* Caso `fullPath` seja invalida ou vazia, `res` recebe string vazia */
 /* Caso `fullPath` nao contenha caminho, sera considerado caminho relativo, e res recebe "./" */
-/* Valor de retorno: ponteiro para `res` */
+/* Valor de retorno: ponteiro para primeiro caracter de `res` */
 /* Ex.: getPath(res, "/home/usuarios/ze/a.txt") -> res recebe "/home/usuarios/ze" */
 
 char *getFileName(char *res, char *fullPath);
 /* Dados uma string valida `res` e um nome de arquivo possivelmente completamente qualificado `fullPath`, armazena apenas o nome do arquivo, incluindo extensao, em `res` */
 /* Caso `fullPath` seja invalida ou vazia, `res` recebe string vazia */
 /* Caso `fullPath` nao contenha nome de arquivo (ou seja, termine em '/'), `res` recebe string vazia */
-/* Valor de retorno: ponteiro para `res` */
+/* Valor de retorno: ponteiro para primeiro caracter de `res` */
 /* Ex.: getFileName(res, "/home/usuarios/ze/a.txt") -> res recebe "a.txt" */
 
 char *getSuffix(char *res, char *fullPath);
@@ -23,7 +23,7 @@ char *getSuffix(char *res, char *fullPath);
 /* Caso `fullPath` seja invalida ou vazia, `res` recebe string vazia */
 /* Caso `fullPath` nao contenha nome de arquivo (ou seja, termine em '/'), `res` recebe string vazia */
 /* Caso `fullPath` nao contenha extensao, res recebe string vazia */
-/* Valor de retorno: ponteiro para `res` */
+/* Valor de retorno: ponteiro para primeiro caracter de `res` */
 /* Ex.: getSuffix(res, "/home/usuarios/ze/a.txt") -> res recebe "txt" */
 
 int hasSlash(char *path);
@@ -36,7 +36,7 @@ char *concatFileSuffix(char *res, char *fileName, char *suffix);
 /* Caso `fileName` seja invalida ou vazia, `res` recebe string vazia */
 /* Caso `suffix` seja invalida ou vazia, `res` recebe conteudo de `fileName` */
 /* Caso `fileName` ja contenha extensao, nova extensao é concatenada ao final */
-/* Valor de retorno: ponteiro para `res` */
+/* Valor de retorno: ponteiro para primeiro caracter de `res` */
 /* Ex.: concatFileSuffix(res, "a.txt", "old") -> res recebe "a.txt.old" */
 
 char *concatPathFile(char *res, char *path, char *fileName);
@@ -44,7 +44,7 @@ char *concatPathFile(char *res, char *path, char *fileName);
 /* Caso `fileName` seja invalida ou vazia, `res` recebe string vazia */
 /* Caso `path` seja invalida ou vazia, caminho e considerado relativo */
 /* Caso `path` ja contenha arquivo, ou `fileName` ja contenha caminho, `res` recebe string vazia */
-/* Valor de retorno: ponteiro para `res` */
+/* Valor de retorno: ponteiro para primeiro caracter de `res` */
 /* Ex.: concatPathFile(res, "/home/usuarios/ze", "a.txt") -> res recebe "/home/usuarios/ze/a.txt" */
 
 #endif
