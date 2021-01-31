8067dd8ebd37ad9d95d2b0f50c674b5c76928b8e
M0chizuki <mochizuki@belzeen.net>
2021-01-31 14:44:32 -0300

    Add getters and setters

--- Revisao 2 - Os Bravos e Destemidos/src/dataStore.h
+++ Revisao 2 - Os Bravos e Destemidos/src/dataStore.h
@@ -40,4 +40,29 @@ void destroyDataFile(dataFileT **dataFile);
 /* Destroi estrutura do arquivo */
 /* Substitui ponteiro original por NULL para fim de controle */
 
+void setVarX(dataStoreT *dataStore, char *varX); /* Altera valor da variavel x do programa */
+char *getVarX(dataStoreT *dataStore); /* Retorna valor da variavel x do programa */
+
+void setVarY(dataStoreT *dataStore, char *varY); /* Altera valor da variavel y do programa */
+char *getVarY(dataStoreT *dataStore); /* Retorna valor da variavel y do programa */
+
+void setVarZ(dataStoreT *dataStore, char *varZ); /* Altera valor da variavel z do programa */
+char *getVarZ(dataStoreT *dataStore); /* Retorna valor da variavel z do programa */
+
+char *getParam1(dataStoreT *dataStore); /* Retorna valor do parametro 1 do programa */
+char *getParam2(dataStoreT *dataStore); /* Retorna valor do parametro 2 do programa */
+char *getParam3(dataStoreT *dataStore); /* Retorna valor do parametro 3 do programa */
+
+void setVarD(dataStoreT *dataStore, char *varD); /* Altera valor da variavel d do programa */
+char *getVarD(dataStoreT *dataStore); /* Retorna valor da variavel d do programa */
+
+void setVarA(dataStoreT *dataStore, char *varA); /* Altera valor da variavel a do programa */
+char *getVarA(dataStoreT *dataStore); /* Retorna valor da variavel a do programa */
+
+void setVarE(dataStoreT *dataStore, char *varE); /* Altera valor da variavel e do programa */
+char *getVarE(dataStoreT *dataStore); /* Retorna valor da variavel e do programa */
+
+void addFileToList(dataStoreT *dataStore, int id, char *path, char *mode); /* Cria estrutura dataFileT e armazena na lista do programa */
+dataFileT *getFileFromID(dataStoreT *dataStore, int id); /* Retorna estrutura dataFileT com ID requisitado */
+
 #endif
