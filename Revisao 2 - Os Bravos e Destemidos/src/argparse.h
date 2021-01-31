662dae5e980919eb54323922aa19a07f2854884f
M0chizuki <mochizuki@belzeen.net>
2021-01-31 08:35:53 -0300

    Yeet

--- Revisao 2 - Os Bravos e Destemidos/src/argparse.h
+++
@@ -1,6 +0,0 @@
-#ifndef MODULO_ARGPARSE_H
-#define MODULO_ARGPARSE_H
-
-int argumentParse(char* result, int argc, char* argv[], char* target);
-
-#endif
--- Revisao 2 - Os Bravos e Destemidos/src/stringManip.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODULO_STRINGMANIP_H
-#define MODULO_STRINGMANIP_H
-
-char* findCharacter(char *string, char query);
-char* rfindCharacter(char *string, char query);
-
-int validString(char* string);
-
-#endif
