ed8b1b5ecae52cbfc01fe9b929f5451d48bd4913
SSparks31 <mochizuki@belzeen.net>
2021-01-31 12:26:55 -0300

    Now it actually works

--- Revisao 2 - Os Bravos e Destemidos/src/fileHelp.c
+++ Revisao 2 - Os Bravos e Destemidos/src/fileHelp.c
@@ -1,17 +1,17 @@
 #include "fileHelp.h"
 
 #include <sys/stat.h>
-#include <limits.h>
 
-#include <string.h>
+#define DIR_MASK 0777
 
 int makedirs(char* fullPath) {
     char *temp = fullPath;
-    for (; *temp; ++temp); {
+    for (; *temp; ++temp) {
         if (*temp == '/') {
             *temp = '\0';
-            mkdir(fullPath, 777);
+            mkdir(fullPath, DIR_MASK);
             *temp = '/';
         }
     }
+    mkdir(fullPath, DIR_MASK);
 }
