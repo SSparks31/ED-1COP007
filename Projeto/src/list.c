423bf2ea690e09010ffec2112276161df53696fb
SSparks31 <mochizuki@belzeen.net>
2021-03-09 11:23:45 -0300

    Char instead of string

--- Projeto/src/list.c
+++ Projeto/src/list.c
@@ -1,35 +1,27 @@
 #include "list.h"
 
 struct list {
-    char* type;
-    sListT sList;
-    dListT dList;
+    char type;
+    void* list;
 }
 
-ListT createList(int maxElem, char* type) {
+ListT createList(int maxElem, char type) {
     
     listT list = malloc(sizeof(listT));
     if (!list) {
         return NULL;
     }
 
-    list->type = malloc(strlen(tp) + 1);
-    if (!list->type) {
-        free(list);
-        return NULL;
-    }
-    strcpy(list->type, type);
+    list->type = type;
 
-    if (strcmp(type, "sta") == 0) {
-        list->sListT = createSList(maxElem);
-        list->dListT = NULL;
-    } else if (strcmp(type, "dyn") == 0) {
-        list->sListT = NULL;
-        list->dListT = createDList(maxElem);
+    if (type == 's') {
+        list->list = createSList(maxElem);
+    } else if (type == 'd'){
+        list->l = NULL;
+        list->l = createDList(maxElem);
     }
 
-    if (!list->sListT && !list->dListT) {
-        free(list->type);
+    if (!list->list) {
         free(list);
         return NULL;
     }
