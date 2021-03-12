abb75112e4075e4d41d19051bad1706af072a68e
SSparks31 <mochizuki@belzeen.net>
2021-03-12 20:48:05 -0300

    Essentially ready, only depending on qry.c

--- Projeto/src/main.c
+++ Projeto/src/main.c
@@ -1,41 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "./progrData/progrData.h"
 
 #include "./geo/geo.h"
-#include "./list/list.h"
-#include "./svg/rect.h"
 
-void printList(listT list) {
-    listPosT aux = getFirstElementList(list);
+#include "./helper/argHelp.h"
+#include "./helper/stringHelp.h"
 
-    while (aux) {
-        printf("[%d] ", *((int*) getElementList(list, aux)));
-        aux = getNextElementList(list, aux);
+int main(int argc, char* argv[]) {
+    char c;
+
+    char* inputPath = NULL;
+    char* outputPath = NULL;
+    char* geoName = NULL;
+    char* qryName = NULL;
+    int collect = 0;
+
+    while ((c = getArguments(argc, argv, "e:f:o:q:ic")) != -1) {
+        switch (c) {
+        case 'e':
+            if (!optarg || optarg[0] == '-') {
+                printf("Opcao -%c necessita de argumento\n", c);
+                return -1;
+            }
+            inputPath = optarg;
+            break;
+        
+        case 'f':
+            if (!optarg || optarg[0] == '-') {
+                printf("Opcao -%c necessita de argumento\n", c);
+                return -1;
+            }
+            if (strcmp(optarg, "b") == 0) {
+                collect = 3;
+            } else {
+                geoName = optarg;
+            }
+            break;
+        
+        case 'o':
+            if (!optarg || optarg[0] == '-') {
+                printf("Opcao -%c necessita de argumento\n", c);
+                return -1;
+            }
+            outputPath = optarg;
+            break;
+
+        case 'q':
+            if (!optarg || optarg[0] == '-') {
+                printf("Opcao -%c necessita de argumento\n", c);
+                return -1;
+            }
+            qryName = optarg;
+            break;
+        
+        case 'i':
+            collect = 1;
+            break;
+        
+        case 'c':
+            collect = 2;
+            break;
+        
+        default:
+            
+            break;
+        }
     }
-    printf("\n");
-}
 
-int main() {
-
-    char* geoPath = "./a1.geo";
-
-    listT geo = geoParser(geoPath);
-
-    listPosT aux = getFirstElementList(geo);
-
-    rectT rect = getElementList(geo, aux);
-
-    while (aux) {
-        printf("%s\n", getIDRect(rect));
-        aux = getNextElementList(geo, aux);
-        rect = getElementList(geo, aux);
+    char* emptyString = "";
+    if (isEmpty(inputPath)) {
+        inputPath = emptyString;
+    }
+    if (isEmpty(outputPath)) {
+        printf("Opcao -o e obrigatoria\n");
+        return -1;
+    }
+    if (isEmpty(geoName)) {
+        printf("Opcao -f e obrigatoria\n");
+        return -1;
+    }
+    if (isEmpty(qryName)) {
+        qryName = emptyString;
     }
 
-    while (!isEmptyList(geo)) {
-        destroyRect(removeList(geo, getFirstElementList(geo)));
-    }
+    progrDataT progrData = createProgrData(inputPath, outputPath, geoName, qryName, collect);
 
-    destroyList(geo);
+    geoParser(progrData);
+
+    // qryParser(progrData);
+
+    destroyProgrData(progrData);
 
     return 0;
 }
