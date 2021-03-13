02f7161be44fa46e68007b4afce69917d41946d7
SSparks31 <mochizuki@belzeen.net>
2021-03-13 20:16:58 -0300

    Support for lines and circles

--- Projeto/src/svg/svg.c
+++ Projeto/src/svg/svg.c
@@ -29,26 +29,141 @@ int addRectToSVG(FILE* svgFile, rectT rect) {
         return -1;
     }
 
-    const char* rectFormat = "<rect x = \"%s\" y = \"%s\" width = \"%s\" height = \"%s\" fill=\"%s\" stroke = \"%s\" fill-opacity=\"%s\" stroke-opacity=\"%s\" stroke-width=\"1\" />\n";
+    const char* rectFormat = "<rect x = \"%s\" y = \"%s\" width = \"%s\" height = \"%s\" fill=\"%s\" stroke = \"%s\" fill-opacity=\"%s\" stroke-opacity=\"%s\" />\n";
+
+    char* x = getXRect(rect);
+    char* y = getYRect(rect);
+    char* width = getWidthRect(rect);
+    char* height = getHeightRect(rect);
+    char* fillColor = getFillColorRect(rect);
+    char* borderColor = getBorderColorRect(rect);
 
     char borderOpacity[4] = "100";
-    if (strcmp(getBorderColorRect(rect), "@") == 0) {
+    if (strcmp(borderColor, "@") == 0) {
         strcpy(borderOpacity, "0");
     }
 
     char fillOpacity[4] = "100";
-    if (strcmp(getFillColorRect(rect), "@") == 0) {
+    if (strcmp(fillColor, "@") == 0) {
         strcpy(fillOpacity, "0");
     }
 
-    char* xRect = getXRect(rect);
-    char* yRect = getYRect(rect);
-    char* widthRect = getWidthRect(rect);
-    char* heightRect = getHeightRect(rect);
-    char* fillColorRect = getFillColorRect(rect);
-    char* borderColorRect = getBorderColorRect(rect);
+    fprintf(svgFile, rectFormat, x, y, width, height, fillColor, borderColor, fillOpacity, borderOpacity);
+    return 0;
+}
 
-    fprintf(svgFile, rectFormat, xRect, yRect, widthRect, heightRect, fillColorRect, borderColorRect, fillOpacity, borderOpacity);
+int addDottedRectToSVG(FILE* svgFile, rectT rect) {
+    if (!svgFile || !rect) {
+        return -1;
+    }
+
+    const char* rectFormat = "<rect x = \"%s\" y = \"%s\" width = \"%s\" height = \"%s\" fill=\"%s\" stroke = \"%s\" fill-opacity=\"%s\" stroke-opacity=\"%s\" stroke-width=\"2\" stroke-dasharray=\"2\" />\n";
+
+    char* x = getXRect(rect);
+    char* y = getYRect(rect);
+    char* width = getWidthRect(rect);
+    char* height = getHeightRect(rect);
+    char* fillColor = getFillColorRect(rect);
+    char* borderColor = getBorderColorRect(rect);
+
+    char borderOpacity[4] = "100";
+    if (strcmp(borderColor, "@") == 0) {
+        strcpy(borderOpacity, "0");
+    }
+
+    char fillOpacity[4] = "100";
+    if (strcmp(fillColor, "@") == 0) {
+        strcpy(fillOpacity, "0");
+    }
+
+    fprintf(svgFile, rectFormat, x, y, width, height, fillColor, borderColor, fillOpacity, borderOpacity);
+    return 0;
+}
+
+int addLineToSVG(FILE* svgFile, lineT line) {
+    if (!svgFile || !line) {
+        return -1;
+    }
+
+    const char* lineFormat = "<line x1 = \"%s\" y1 = \"%s\" x2 = \"%s\" y2 = \"%s\" stroke = \"%s\" stroke-width=\"1\" />\n";
+
+    char* color = getColorLine(line);
+    char* x1 = getX1Line(line);
+    char* y1 = getY1Line(line);
+    char* x2 = getX2Line(line);
+    char* y2 = getY2Line(line);
+
+    fprintf(svgFile, lineFormat, x1, y1, x2, y2, color);
+}
+
+int addDottedLineToSVG(FILE* svgFile, lineT line) {
+    if (!svgFile || !line) {
+        return -1;
+    }
+
+    const char* lineFormat = "<line x1=\"%s\" y1=\"%s\" x2=\"%s\" y2=\"%s\" stroke=\"%s\"  stroke-width=\"1\" stroke-dasharray=\"1\" />\n";
+
+    char* color = getColorLine(line);
+    char* x1 = getX1Line(line);
+    char* y1 = getY1Line(line);
+    char* x2 = getX2Line(line);
+    char* y2 = getY2Line(line);
+
+    fprintf(svgFile, lineFormat, x1, y1, x2, y2, color);
+    return 0;
+}
+
+int addCircleToSVG(FILE* svgFile, circleT circle) {
+    if (!svgFile || !circle) {
+        return -1;
+    }
+
+    const char* circleFormat = "<circle cx=\"%s\" cy=\"%s\" r=\"%s\" stroke=\"%s\" fill=\"%s\" stroke-width=\"1\" stroke-opacity=\"%s\" fill-opacity=\"%s\" />\n";
+
+    char* borderColor = getBorderColorCircle(circle); 
+    char* fillColor = getFillColorCircle(circle); 
+    char* xCenter = getXCenterCircle(circle); 
+    char* yCenter = getYCenterCircle(circle); 
+    char* radius = getRadiusCircle(circle);
+
+    char borderOpacity[4] = "100";
+    if (strcmp(borderColor, "@") == 0) {
+        strcpy(borderOpacity, "0");
+    }
+
+    char fillOpacity[4] = "100";
+    if (strcmp(fillColor, "@") == 0) {
+        strcpy(fillOpacity, "0");
+    }
+
+    fprintf(svgFile, circleFormat, xCenter, yCenter, radius, borderColor, fillColor, borderOpacity, fillOpacity);
+    return 0;
+}
+
+int addDottedCircleToSVG(FILE* svgFile, circleT circle) {
+    if (!svgFile || !circle) {
+        return -1;
+    }
+
+    const char* circleFormat = "<circle cx=\"%s\" cy=\"%s\" r=\"%s\" stroke=\"%s\" fill=\"%s\" stroke-width=\"1\" stroke-opacity=\"%s\" fill-opacity=\"%s\" stroke-dasharray=\"1\" />\n";
+
+    char* borderColor = getBorderColorCircle(circle); 
+    char* fillColor = getFillColorCircle(circle); 
+    char* xCenter = getXCenterCircle(circle); 
+    char* yCenter = getYCenterCircle(circle); 
+    char* radius = getRadiusCircle(circle); 
+
+    char borderOpacity[4] = "100";
+    if (strcmp(borderColor, "@") == 0) {
+        strcpy(borderOpacity, "0");
+    }
+
+    char fillOpacity[4] = "100";
+    if (strcmp(fillColor, "@") == 0) {
+        strcpy(fillOpacity, "0");
+    }
+
+    fprintf(svgFile, circleFormat, xCenter, yCenter, radius, borderColor, fillColor, borderOpacity, fillOpacity);
     return 0;
 }
 
--- Projeto/src/svg/svg.h
+++ Projeto/src/svg/svg.h
@@ -4,10 +4,22 @@
 #include <stdio.h>
 
 #include "./rect.h"
+#include "./line.h"
+#include "./circle.h"
 
 FILE* startSVG(char* path, char* fileName);
 
 int addRectToSVG(FILE* svgFile, rectT rect);
+
+int addDottedRectToSVG(FILE* svgFile, rectT rect);
+
+int addLineToSVG(FILE* svgFile, lineT line);
+
+int addDottedLineToSVG(FILE* svgFile, lineT line);
+
+int addCircleToSVG(FILE* svgFile, circleT circle);
+
+int addDottedCircleToSVG(FILE* svgFile, circleT circle);
 
 int finishSVG(FILE* svgFile);
 
