9af5dd789612412e0d88f3b28f7ff5327d05d185
M0chizuki <mochizuki@belzeen.net>
2021-03-13 20:39:01 -0300

    Comment

--- Projeto/src/geo/geo.h
+++ Projeto/src/geo/geo.h
@@ -8,5 +8,8 @@
 #include "../list/list.h"
 
 void geoParser(progrDataT progrData);
+/* Dada a struct padrao de armazenamento de dados `progrData`, analisa e executa comandos lidos no arquivo .geo indicado pelo argumento -f */
+/* Responsavel por inicializar a lista de retangulos presente em `progrData` */
+/* Caso `progrData` seja invalida, nao realiza nenhuma acao */
 
 #endif
