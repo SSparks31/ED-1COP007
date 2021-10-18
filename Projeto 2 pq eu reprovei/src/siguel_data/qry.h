d9ee4041d8e38bfa562a597dc3753c06691e02f3
SSparks31 <mochizuki@belzeen.net>
2021-10-18 08:18:42 -0300

    asdasdsadsa

--- Projeto 2 pq eu reprovei/src/siguel_data/qry.h
+++ Projeto 2 pq eu reprovei/src/siguel_data/qry.h
@@ -3,6 +3,6 @@
 
 #include "../list/list.h"
 
-void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, List circles, List lines, List rectangles, List texts);
+void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, List shapes);
 
 #endif
