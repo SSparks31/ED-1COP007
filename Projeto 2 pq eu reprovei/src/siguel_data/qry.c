ba54124d8d668b58a9be3cc8a5b9e4c0b0c2fdf5
M0chizuki <mochizuki@belzeen.net>
2021-10-18 07:29:16 -0300

    asdasadsa

--- Projeto 2 pq eu reprovei/src/siguel_data/qry.c
+++ Projeto 2 pq eu reprovei/src/siguel_data/qry.c
@@ -14,8 +14,7 @@ void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, List circl
 
     char* qry_path = concatPathFile(BED, qry_name);
     FILE* qry_file = fopen(qry_path, "r");
-    printf("%s\n", qry_path);
-    free(qry_path);
+    // free(qry_path);
       
     
     char* geo_no_suffix = stripSuffix(geo_name);
@@ -23,4 +22,9 @@ void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, List circl
     char* svg_name = malloc(strlen(geo_no_suffix) + strlen("-") + strlen(qry_no_suffix) + strlen(".svg") + 1);
     sprintf(svg_name, "%s-%s.svg");
     char* svg_path = concatPathFile(BSD, svg_path);
-    printf("%s\n", svg_path);
+    char* txt_name = stripSuffix(svg_path);
+    char* txt_path = concatPathFile(txt_name, ".txt");
+    printf("%s %s %s\n", qry_path, svg_path, txt_path);
+
+    return;
+}
